795201ba4e595cea99b461d3acf29500a247bf81
Кристина <kristina.misun@mail.ru>
2022-10-19 19:49:12 +0300

    homework

--- task02.py
+++ task02.py
@@ -1,28 +1,26 @@
 # Разработайте программу, которая проверяет, что все цифры заданного
 # натурального числа различны
 
-def different_digits(number):
+def same_digits(number):
     digit_1 = 0
     digit_2 = 0
-    result = False
     while number > 0:
         digit_1 = number % 10
         number = number // 10
-        while number > 0:
+        if number != 0:
             digit_2 = number % 10
-            number = number // 10
             if digit_1 != digit_2:
-                result = True
-                # number = number // 10
-    return result
+                number = number = number // 10
+        else:
+            break
 
-    # if digit_1 > 0:
-    #     number = number // 10
+    return True if digit_1 != digit_2 else False
+
 
 def main():
     number = int(input("Input your number: "))
 
-    result = different_digits(number)
+    result = same_digits(number)
 
     msg = f"All digits of number {number} are different - it is {result}."
     print(msg)
